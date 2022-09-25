0c0d824e9c1c1a7bead87bbb4fb97f984b2cc615
hjinnkim <khj12203@gmail.com>
2022-09-25 16:53:44 +0900

    Add favicon

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories1"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories1/post-name-here/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories2"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories2/post-name-here-2/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories3"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories3/post-name-here-3/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories4"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories4/post-name-here-4/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories5"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories5/post-name-here-5/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories6"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories6/post-name-here-6/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Categories7"],
+        "tags": ["tag1","tag2"],
+        "url": "/categories7/post-name-here-7/",
+        "teaser": null
+      }]
